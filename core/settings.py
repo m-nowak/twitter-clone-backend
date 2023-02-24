48111ddf50ae5007b05b4f475fe55a50e17bb151
m-nowak <48589236+m-nowak@users.noreply.github.com>
2023-02-24 14:15:41 +0000

    v2

--- core/settings.py
+++ core/settings.py
@@ -132,10 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
