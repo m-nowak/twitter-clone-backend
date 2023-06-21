3efd6a006fa77327d195a9ab2915e9c77eb5001d
Michal Nowak <48589236+m-nowak@users.noreply.github.com>
2023-06-21 11:08:08 +0100

    Add files via upload

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = cfg('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = cfg('DEBUG')
 
-ALLOWED_HOSTS = ['twitter-clone-backend-production-c55e.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
